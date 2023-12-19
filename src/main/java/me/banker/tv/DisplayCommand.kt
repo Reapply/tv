4f5a16e3dea97934d89ac959e699268954f03d68
Banker <smssapmmer12349@gmail.com>
2023-12-18 22:46:58 -0500

    Remove GIF display support in command
    The 'gif' command case from the `DisplayCommand` class was removed. This means GIF files cannot be handled or displayed anymore. The now deleted code contained all routines related to reading, scaling, displaying, and cycling through frames of a specified GIF file. The 'off' command which removes the TextDisplays, however, is still present.

--- src/main/java/me/banker/tv/DisplayCommand.kt
+++ src/main/java/me/banker/tv/DisplayCommand.kt
@@ -16,6 +16,8 @@ import javax.imageio.stream.ImageInputStream
 import org.bukkit.Color
 import org.bukkit.entity.Display
 
+/**
+ * The DisplayCommand class is responsible for handling the*/
 class DisplayCommand(private val plugin: Tv) : CommandExecutor {
     private var textDisplays: List<TextDisplay>? = null
 
@@ -85,70 +87,7 @@ class DisplayCommand(private val plugin: Tv) : CommandExecutor {
                     sender.sendMessage("Image displayed.")
                 }
 
-                "gif" -> {
-                    val filename = args[1]
-                    val file = File(plugin.dataFolder, "images/$filename.gif")
 
-                    if (!file.exists()) {
-                        sender.sendMessage("File not found: $filename.gif")
-                        return true
-                    }
-
-                    val imageReader: ImageReader = ImageIO.getImageReadersBySuffix("gif").next()
-                    val imageInputStream: ImageInputStream = ImageIO.createImageInputStream(file)
-                    imageReader.setInput(imageInputStream, false)
-
-                    val numImages = imageReader.getNumImages(true)
-
-                    // Remove the old TextDisplays
-                    textDisplays?.forEach { it.remove() }
-
-                    for (i in 0 until numImages) {
-                        var image: BufferedImage = imageReader.read(i)
-
-                        // Scale down the image to 100x100
-                        val scaledImage = image.getScaledInstance(150, 100, Image.SCALE_DEFAULT)
-                        image = BufferedImage(150, 100, BufferedImage.TYPE_INT_RGB)
-                        image.graphics.drawImage(scaledImage, 0, 0, null)
-
-                        val width = image.width
-                        val height = image.height
-
-                        val displays = mutableListOf<TextDisplay>()
-
-                        for (x in 0 until width) {
-                            for (y in 0 until height) {
-                                val color = image.getRGB(x, y)
-                                val red = color shr 16 and 0xFF
-                                val green = color shr 8 and 0xFF
-                                val blue = color and 0xFF
-
-                                // Offset location, decrease the divisor to move closer and invert y for correct orientation
-                                val location = sender.location.add(x.toDouble()/10.0, (height - y).toDouble()/10.0, 0.0)
-
-                                val textDisplay = sender.world.spawn(location, TextDisplay::class.java).apply {
-                                    // Set the rgb color of the text display as the settext
-                                    this.setText(" ")
-                                    this.setLineWidth(1)
-                                    this.setSeeThrough(false)
-                                    this.setBackgroundColor(Color.fromRGB(red, green, blue))
-                                }
-
-                                displays.add(textDisplay)
-
-                                // Move back player to original position for next spawn
-                                location.subtract(x.toDouble()/10.0, (height - y).toDouble()/10.0, 0.0)
-                            }
-                        }
-
-                        textDisplays = displays
-
-                        // Delay between frames
-                        Thread.sleep(100)
-                    }
-
-                    sender.sendMessage("GIF displayed.")
-                }
 
                 "off" -> {
                     // Remove the TextDisplays
