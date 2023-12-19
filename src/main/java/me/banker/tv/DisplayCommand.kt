c6bc131ffa760a53c3d0d4ece885f49ef1df4cff
Banker <smssapmmer12349@gmail.com>
2023-12-18 22:29:27 -0500

    Add support for GIF display in command
    Updated the `DisplayCommand` class to handle and display GIF files. A new 'gif' command case was added in the function `onCommand()`, allowing users to specify a GIF file to display. This function now reads and iterates over each image in a GIF file, displaying them sequentially to create the GIF animation. The delay between frames is 100 milliseconds.

--- src/main/java/me/banker/tv/DisplayCommand.kt
+++ src/main/java/me/banker/tv/DisplayCommand.kt
@@ -11,27 +11,14 @@ import java.awt.Image
 import java.io.File
 import java.util.*
 import javax.imageio.ImageIO
+import javax.imageio.ImageReader
+import javax.imageio.stream.ImageInputStream
 import org.bukkit.Color
 import org.bukkit.entity.Display
 
-/**
- * The DisplayCommand class is an implementation of the CommandExecutor interface
- * that handles the /display command.
- *
- * @param plugin The Tv plugin instance
- */
 class DisplayCommand(private val plugin: Tv) : CommandExecutor {
-    private var textDisplay: TextDisplay? = null
+    private var textDisplays: List<TextDisplay>? = null
 
-    /**
-     * Handles the "display" command.
-     *
-     * @param sender The sender of the command.
-     * @param command The command that was executed.
-     * @param label The alias used for the command.
-     * @param args The arguments passed with the command.
-     * @return true if the command was executed successfully, false otherwise.
-     */
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
         if (command.name.equals("display", ignoreCase = true)) {
             if (sender !is Player) {
@@ -40,7 +27,7 @@ class DisplayCommand(private val plugin: Tv) : CommandExecutor {
             }
 
             if (args.size < 2) {
-                sender.sendMessage("Please specify 'on' or 'off' and a filename.")
+                sender.sendMessage("Please specify 'on', 'gif' or 'off' and a filename.")
                 return true
             }
 
@@ -57,14 +44,16 @@ class DisplayCommand(private val plugin: Tv) : CommandExecutor {
 
                     // Scale down the image to 100x100
                     val scaledImage = image.getScaledInstance(150, 100, Image.SCALE_DEFAULT)
-                    image = BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB)
+                    image = BufferedImage(150, 100, BufferedImage.TYPE_INT_RGB)
                     image.graphics.drawImage(scaledImage, 0, 0, null)
 
                     val width = image.width
                     val height = image.height
 
                     // Remove the old TextDisplay
-                    textDisplay?.remove()
+                    textDisplays?.forEach { it.remove() }
+
+                    val displays = mutableListOf<TextDisplay>()
 
                     for (x in 0 until width) {
                         for (y in 0 until height) {
@@ -76,7 +65,7 @@ class DisplayCommand(private val plugin: Tv) : CommandExecutor {
                             // Offset location, decrease the divisor to move closer and invert y for correct orientation
                             val location = sender.location.add(x.toDouble()/10.0, (height - y).toDouble()/10.0, 0.0)
 
-                            textDisplay = sender.world.spawn(location, TextDisplay::class.java).apply {
+                            val textDisplay = sender.world.spawn(location, TextDisplay::class.java).apply {
                                 // Set the rgb color of the text display as the settext
                                 this.setText(" ")
                                 this.setLineWidth(1)
@@ -84,23 +73,91 @@ class DisplayCommand(private val plugin: Tv) : CommandExecutor {
                                 this.setBackgroundColor(Color.fromRGB(red, green, blue))
                             }
 
+                            displays.add(textDisplay)
+
                             // Move back player to original position for next spawn
                             location.subtract(x.toDouble()/10.0, (height - y).toDouble()/10.0, 0.0)
                         }
                     }
 
+                    textDisplays = displays
 
                     sender.sendMessage("Image displayed.")
                 }
 
-                "off" -> {
-                    // Remove the TextDisplay
-                    textDisplay?.remove()
-                    textDisplay = null
-                    sender.sendMessage("Image removed.")
+                "gif" -> {
+                    val filename = args[1]
+                    val file = File(plugin.dataFolder, "images/$filename.gif")
+
+                    if (!file.exists()) {
+                        sender.sendMessage("File not found: $filename.gif")
+                        return true
+                    }
+
+                    val imageReader: ImageReader = ImageIO.getImageReadersBySuffix("gif").next()
+                    val imageInputStream: ImageInputStream = ImageIO.createImageInputStream(file)
+                    imageReader.setInput(imageInputStream, false)
+
+                    val numImages = imageReader.getNumImages(true)
+
+                    // Remove the old TextDisplays
+                    textDisplays?.forEach { it.remove() }
+
+                    for (i in 0 until numImages) {
+                        var image: BufferedImage = imageReader.read(i)
+
+                        // Scale down the image to 100x100
+                        val scaledImage = image.getScaledInstance(150, 100, Image.SCALE_DEFAULT)
+                        image = BufferedImage(150, 100, BufferedImage.TYPE_INT_RGB)
+                        image.graphics.drawImage(scaledImage, 0, 0, null)
+
+                        val width = image.width
+                        val height = image.height
+
+                        val displays = mutableListOf<TextDisplay>()
+
+                        for (x in 0 until width) {
+                            for (y in 0 until height) {
+                                val color = image.getRGB(x, y)
+                                val red = color shr 16 and 0xFF
+                                val green = color shr 8 and 0xFF
+                                val blue = color and 0xFF
+
+                                // Offset location, decrease the divisor to move closer and invert y for correct orientation
+                                val location = sender.location.add(x.toDouble()/10.0, (height - y).toDouble()/10.0, 0.0)
+
+                                val textDisplay = sender.world.spawn(location, TextDisplay::class.java).apply {
+                                    // Set the rgb color of the text display as the settext
+                                    this.setText(" ")
+                                    this.setLineWidth(1)
+                                    this.setSeeThrough(false)
+                                    this.setBackgroundColor(Color.fromRGB(red, green, blue))
+                                }
+
+                                displays.add(textDisplay)
+
+                                // Move back player to original position for next spawn
+                                location.subtract(x.toDouble()/10.0, (height - y).toDouble()/10.0, 0.0)
+                            }
+                        }
+
+                        textDisplays = displays
+
+                        // Delay between frames
+                        Thread.sleep(100)
+                    }
+
+                    sender.sendMessage("GIF displayed.")
                 }
 
-                else -> sender.sendMessage("Invalid argument. Please specify 'on' or 'off'.")
+                "off" -> {
+                    // Remove the TextDisplays
+                    textDisplays?.forEach { it.remove() }
+                    textDisplays = null
+                    sender.sendMessage("Display removed.")
+                }
+
+                else -> sender.sendMessage("Invalid argument. Please specify 'on', 'gif' or 'off'.")
             }
 
             return true
